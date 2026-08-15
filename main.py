a5c54f2443d1a52951e519929d2dc1e8924730a7
Kevin <kh925063@gmail.com>
2026-08-14 23:59:21 -0600

    Front end creado

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from fastapi import APIRouter, Depends, FastAPI, Header, HTTPException, status
-from routers import estudiantes
+from fastapi import APIRouter, Depends, FastAPI, Header, HTTPException, status, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+from routers import estudiantes, registrar  # 👈 1. Importamos el router de registrar
 from models.almacen import miClaseBase, motor
 
 app = FastAPI()
 
-miClaseBase.metadata.create_all(bind = motor)
 
+miClaseBase.metadata.create_all(bind=motor)
+
+#  Montar la carpeta de archivos estaticos (JS, CSS, Imagenes)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#  Configurar la carpeta de las plantillas HTML
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def login(request: Request):
+    return templates.TemplateResponse(request, 'login.html')
+
+
+@app.get("/signup")
+def mostrar_registrasre(request: Request):
+    return templates.TemplateResponse(request,'signUp.html')
+
+@app.get("/general")
+def general(request: Request):
+    return templates.TemplateResponse(request, 'general.html')
+
+
+# 👈 5. Incluir ambos routers
 app.include_router(estudiantes.router)
+app.include_router(registrar.router)  # 👈 Añadido el router de registro
---
+++ static/signUp.js
@@ -0,0 +1,32 @@
+document.getElementById("registrar").addEventListener("submit", async function(e) {
+
+    e.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const telefono = document.getElementById("telefono").value;
+    const correo = document.getElementById("correo").value;
+    const cargo = document.getElementById("cargo").value;
+    const user = document.getElementById("user").value;
+    const password = document.getElementById("password").value;
+
+
+    const respuesta = await fetch("/registrar/", { 
+
+        method: "POST", 
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nombre: nombre, telefono: telefono, correo: correo, cargo: cargo, user: user, password: password })
+
+    });
+
+  
+const datos = await respuesta.json();
+
+if (respuesta.ok) {
+    window.location = "/general";
+} else {
+    
+    alert("Error de validación:\n" + JSON.stringify(datos.detail, null, 2));
+    console.log(datos.detail);
+}
+
+});
