76a643192f4966c554fda81c8db1acee1411dc00
Kevin <kh925063@gmail.com>
2026-08-16 13:48:12 -0600

    de Bcrypt a Passlib

--- utils/hash.py
+++ utils/hash.py
@@ -1,31 +1,16 @@
-import bcrypt
+from passlib.context import CryptContext
+
+contexto = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hashear_contrasena(contrasena: str) -> str:
-   
-    # 1. Convertir texto a bytes (bcrypt trabaja con bytes)
-    contrasena_bytes = contrasena.encode('utf-8')
     
-    # 2. Generar un "salt" aleatorio con 12 rondas
-    salt = bcrypt.gensalt(rounds=12)
+    hash_contrasena = contexto.hash(contrasena)
     
-    # 3. Hashear: combina contraseña + salt (una formula por asi decirlo para mezclar textos y palabras)
-    hash_contrasena = bcrypt.hashpw(contrasena_bytes, salt)
-    
-    # 4. Convertir de bytes a texto (para guardar en BD)
-    hash_string = hash_contrasena.decode('utf-8')
-    
-    return hash_string
+    return hash_contrasena
 
 
 def verificar_contrasena(contrasena: str, hash_guardado: str) -> bool:
-   
-    # 1. Convertir ambas a bytes
-    contrasena_bytes = contrasena.encode('utf-8')
-    hash_bytes = hash_guardado.encode('utf-8')
     
-    # 2. Usar bcrypt.checkpw() para verificar
-    #    Compara la contraseña contra el hash
-    resultado = bcrypt.checkpw(contrasena_bytes, hash_bytes)
+    resultado = contexto.verify(contrasena, hash_guardado)
     
-    # 3. Retornar True o False
     return resultado
