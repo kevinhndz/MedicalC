110255256a01184bc64345d79b86220735eded5d
Kevin <kh925063@gmail.com>
2026-08-16 13:57:18 -0600

    Anadiendo nuevo acceso en autenticacion.py

--- utils/autenticacion.py
+++ utils/autenticacion.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI , Depends, Header, HTTPException, status
+from fastapi import FastAPI, Depends, Header, HTTPException, status
 from utils.boletos import verificar_boleto
-
 
 
 def el_vigilante(token: str = Header(...)):
@@ -11,17 +10,17 @@ def el_vigilante(token: str = Header(...)):
         return datos
     else:
         raise HTTPException(
-            status_code= status.HTTP_401_UNAUTHORIZED,
-            detail = "Boleto no valido"
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Boleto no valido"
         )
 
 
-def permiso_admin(info_usuario:dict = Depends(el_vigilante)):
+def permiso_admin(info_usuario: dict = Depends(el_vigilante)):
     
-    if info_usuario["rol"] != "Administrador":
+    if info_usuario["user_rol"] != "Administrador":
         raise HTTPException(
-            status_code= status.HTTP_403_FORBIDDEN,
-            detail= "No tienes permitido hacer esta accion!"
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="No tienes permitido hacer esta accion!"
         )
     else:
         return info_usuario
@@ -29,11 +28,15 @@ def permiso_admin(info_usuario:dict = Depends(el_vigilante)):
 
 def permiso_profesor(info_usuario: dict = Depends(el_vigilante)):
     
-    if info_usuario["rol"]!= "Profesor":
+    if info_usuario["user_rol"] != "Profesor":
         raise HTTPException(
-                    status_code= status.HTTP_403_FORBIDDEN,
-                    detail= "No tienes permitido hacer esta accion!"
-                )
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="No tienes permitido hacer esta accion!"
+        )
     else:
         return info_usuario
-        
+
+
+def cualquier_usuario(info_usuario: dict = Depends(el_vigilante)):
+    
+    return info_usuario
