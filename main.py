cea5583fbcfa13092e97c1357a9cc7dd12a1ea8f
Kevin <kh925063@gmail.com>
2026-08-28 17:52:05 -0600

    primero endpoints funcionando sin hash

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from routers.crearUsuarios import crear
+from base_datos.almacen import motor, miclaseBase
 
-
+miclaseBase.metadata.create_all(bind=motor)
 
 app = FastAPI()
 
+app.include_router(crear.router)
 
-@app.get("/")
-def home():
-    return {"testing": "API"}
