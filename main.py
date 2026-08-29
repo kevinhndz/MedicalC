d882f7b282b7653fd19d3f2b2a60d53c4fdbfa41
Kevin <kh925063@gmail.com>
2026-08-29 13:37:08 -0600

    anadiendo CRUD de medicamentos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routers.crearUsuarios import crear
-from routers.endpoints import pacientes
+from routers.endpoints import pacientes, med
 from routers.Login import login
 from base_datos.almacen import motor, miclaseBase
 
@@ -12,4 +12,5 @@ app = FastAPI()
 app.include_router(crear.router)
 app.include_router(login.router)
 app.include_router(pacientes.router)
+app.include_router(med.router)
 
